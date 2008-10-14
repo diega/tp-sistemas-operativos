c7b734a5cbc6b1d37eb15f1b3d4ea84eee872e2f
diego <diego@27df4b56-31a2-465d-b204-98f02cb8f8aa>
2008-10-14 04:54:08 +0000

    agrego modulos
    git-svn-id: http://www.locolandia.com.ar/svn/tp-sistemas-operativos/trunk@6 27df4b56-31a2-465d-b204-98f02cb8f8aa

---
+++ keyboard_leds_module.c
@@ -0,0 +1,71 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <linux/init.h>
+#include <asm/uaccess.h>
+#include <linux/tty.h>
+#include <linux/kd.h>
+#include <linux/console_struct.h>
+#include <linux/vt_kern.h>
+
+#define procfs_name "keyboard_leds"
+
+struct proc_dir_entry* proc_file;
+struct tty_operations* keyboard_operations;
+
+int procfile_write(struct file* file, const char* buffer, unsigned long count, void* data){
+    char selected_led;
+
+    if(copy_from_user(&selected_led, buffer, 1)){
+        return -EFAULT;
+    }
+    switch(selected_led){
+    case '1':
+        printk(KERN_ALERT "PRESIONA 1\n");
+	(keyboard_operations->ioctl) (vc_cons[fg_console].d->vc_tty, NULL, KDSETLED, LED_NUM);
+        break;
+    case '2':
+        printk(KERN_ALERT "PRESIONA 2\n");
+	(keyboard_operations->ioctl) (vc_cons[fg_console].d->vc_tty, NULL, KDSETLED, LED_CAP);
+        break;
+    case '3':
+        printk(KERN_ALERT "PRESIONA 3\n");
+	(keyboard_operations->ioctl) (vc_cons[fg_console].d->vc_tty, NULL, KDSETLED, LED_SCR);
+        break;
+    default:
+        printk(KERN_ALERT "INPUT DESCONOCIDO\n");
+    }
+    return 3;
+} 
+
+
+static int __init load_module(void){
+    if((proc_file = create_proc_entry(procfs_name, 0664, NULL)) == NULL){
+        remove_proc_entry(procfs_name, NULL);
+        printk(KERN_ALERT "Error: Could not initialize /proc/%s\n", procfs_name);
+        return -ENOMEM;
+    }
+
+    proc_file->owner = THIS_MODULE;
+    proc_file->mode  = S_IFREG | S_IRUGO;
+    proc_file->uid   = 0;
+    proc_file->gid   = 0;
+    proc_file->size  = 37;
+    proc_file->write_proc = procfile_write;
+    printk(KERN_INFO "/proc/%s created\n", procfs_name);
+
+    keyboard_operations = vc_cons[fg_console].d->vc_tty->ops;
+    return 0;
+}
+
+static void __exit remove_module(void){
+    remove_proc_entry(procfs_name, NULL);
+    printk(KERN_INFO "/proc/%s removed\n", procfs_name);
+}
+
+module_init(load_module)
+module_exit(remove_module)
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Sistemas Operativos - Grupo XX");
+MODULE_DESCRIPTION("Ejercicio 3.6.6 - permite prender o apager cada LED del teclado escribiendo en /proc/keyboard_leds");
