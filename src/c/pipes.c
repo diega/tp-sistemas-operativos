7874832f39d01935c45954ac397f70b6052d1e3e
diego <diego@27df4b56-31a2-465d-b204-98f02cb8f8aa>
2008-12-14 12:47:52 +0000

    saco acentos porque quedan mal en latex cuando se incluye el source
    git-svn-id: http://www.locolandia.com.ar/svn/tp-sistemas-operativos/trunk@37 27df4b56-31a2-465d-b204-98f02cb8f8aa

--- src/c/pipes.c
+++ src/c/pipes.c
@@ -2,8 +2,9 @@
 #include<unistd.h>
 #include<sys/types.h>
 
+//acentos omitidos por integracion con latex
 void critical_zone(char* process_name){
-    printf("zona crítica ejecutada por proceso: %s\n", process_name);
+    printf("zona critica ejecutada por proceso: %s\n", process_name);
     sleep(2);
 }
 
@@ -16,19 +17,19 @@ int main(int argc, char** argv){
 
     if(0 == fork()){
         close(fds[0]);
-        printf("hijo1 ejecuta zona crítica\n");
+        printf("hijo1 ejecuta zona critica\n");
         sleep(1);
         critical_zone("hijo1");
-        printf("hijo1 termina de ejecutar zona crítica\n");
+        printf("hijo1 termina de ejecutar zona critica\n");
         write(fds[1], message, (strlen(message)+1));
     } else {
         if(0 == fork()){
             close(fds[1]);
-            printf("hijo2 intenta ejecutar zona crítica\n");
+            printf("hijo2 intenta ejecutar zona critica\n");
             read(fds[0], readbuffer, sizeof(readbuffer));
-            printf("hijo2 ejecuta zona crítica\n");
+            printf("hijo2 ejecuta zona cr'itica\n");
             critical_zone("hijo2");
-            printf("hijo2 termina de ejecutar zona crítica\n");
+            printf("hijo2 termina de ejecutar zona critica\n");
         } else {
             close(fds[0]);
             close(fds[1]);
